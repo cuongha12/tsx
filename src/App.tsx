7248cd449c4af7dcd592e19e0e41e0ceb59c098e
cuongha12 <cuongbeeg1234@gmail.com>
2022-12-12 00:26:06 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Table } from 'antd';
 
-function App() {
+interface DataType {
+  key: number;
+  name: string;
+  image: string;
+  price: number;
+  sale: number;
+  category_id: number;
+}
+const App: React.FC = () => {
+  const [data, setData] = useState<string[]>([])
+  const fetchApi = async () => {
+    try {
+      return await axios.get('/product/').then((res) => setData(res.data));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const loadData = async () => {
+    await fetchApi()
+  }
+  useEffect(() => {
+    loadData();
+  }, [])
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'key',
+      render: (text: string) => <a>{text}</a>,
+    },
+    {
+      title: 'image',
+      dataIndex: 'image',
+      key: 'key',
+    },
+    {
+      title: 'price',
+      dataIndex: 'price',
+      key: 'key',
+    },
+    {
+      title: 'sale',
+      dataIndex: 'sale',
+      key: 'key',
+    },
+    {
+      title: 'category',
+      dataIndex: 'category',
+      key: 'key',
+    },
+  ];
+  const newData = data.map((e: any) => {
+    return {
+      key: e.id,
+      name: e.name,
+      image: e.image,
+      price: e.price,
+      sale: e.sale,
+      category: e.category_id
+    }
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table columns={columns} dataSource={newData} />
     </div>
   );
 }
